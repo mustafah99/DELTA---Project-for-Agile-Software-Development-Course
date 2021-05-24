c0522a0c9ed7099081dafc251ecc95e6a35e34fd
Mustafa <mustafasaidhelal@gmail.com>
2021-05-24 18:07:03 +0200

    First Commit

---
+++ DELTA - Ballin Out Web Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DELTA___Ballin_Out_Web_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
