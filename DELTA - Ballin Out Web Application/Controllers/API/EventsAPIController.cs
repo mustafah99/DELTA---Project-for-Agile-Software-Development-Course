a4dd8d810edfd7fbeaa7b705c3798dfb6c270355
Mustafa <mustafasaidhelal@gmail.com>
2021-06-04 01:39:28 +0200

    Added API Controller with CRUD Operations using Entity Framework

---
+++ DELTA - Ballin Out Web Application/Controllers/API/EventsAPIController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DELTA___Ballin_Out_Web_Application.Data;
+using DELTA___Ballin_Out_Web_Application.Models;
+
+namespace DELTA___Ballin_Out_Web_Application
+{
+    [Route("api/events")]
+    [ApiController]
+    public class EventsAPIController : ControllerBase
+    {
+        private readonly MvcEventsContext _context;
+
+        public EventsAPIController(MvcEventsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/EventsAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Events>>> GetEvents()
+        {
+            return await _context.Events.ToListAsync();
+        }
+
+        // GET: api/EventsAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Events>> GetEvents(int id)
+        {
+            var events = await _context.Events.FindAsync(id);
+
+            if (events == null)
+            {
+                return NotFound();
+            }
+
+            return events;
+        }
+
+        // PUT: api/EventsAPI/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEvents(int id, Events events)
+        {
+            if (id != events.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(events).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EventsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/EventsAPI
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Events>> PostEvents(Events events)
+        {
+            _context.Events.Add(events);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEvents", new { id = events.Id }, events);
+        }
+
+        // DELETE: api/EventsAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEvents(int id)
+        {
+            var events = await _context.Events.FindAsync(id);
+            if (events == null)
+            {
+                return NotFound();
+            }
+
+            _context.Events.Remove(events);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EventsExists(int id)
+        {
+            return _context.Events.Any(e => e.Id == id);
+        }
+    }
+}
