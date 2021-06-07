2e83050152a08c7803efa69e43cb6d372b3ad69e
Mustafa <mustafasaidhelal@gmail.com>
2021-06-07 15:55:22 +0200

    Added a "Coming Soon" page to Contact View to Twitter and Snapchat Links

---
+++ DELTA - Ballin Out Web Application/Controllers/TemporaryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DELTA___Ballin_Out_Web_Application.Controllers
+{
+    public class TemporaryController : Controller
+    {
+        public IActionResult ComingSoon()
+        {
+            return View();
+        }
+    }
+}
